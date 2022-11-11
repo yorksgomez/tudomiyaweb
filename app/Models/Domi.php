2d0d03033268cfb5fe54b6321528ccabc858b470
Yorks Gomez <yorksgomez@gmail.com>
2022-11-11 11:02:51 -0500

    Cambios

--- app/Models/Domi.php
+++ app/Models/Domi.php
@@ -10,7 +10,7 @@ class Domi extends Model
     use HasFactory;
 
     public function user() {
-        return $this->morphOne(User::class, "roleable");
+        return $this->morphOne(User::class, "roleable", "role_type");
     }
 
 
